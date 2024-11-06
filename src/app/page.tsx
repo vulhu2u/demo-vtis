010c8a411a02d383b50c7086d58598704871e660
lehoangvuvt <hoangvule100@gmail.com>
2024-11-06 22:59:58 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import HomeSection1 from "./HomeSection1";
 import HomeSection2 from "./HomeSection2";
@@ -6,58 +6,67 @@ import HomeSection3 from "./HomeSection3";
 import HomeSection4 from "./HomeSection4";
 import HomeSection5 from "./HomeSection5";
 import HomeSection6 from "./HomeSection6";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const observers = []
-    const elements = document.getElementsByClassName("animated-text")
+    const observers: IntersectionObserver[] = [];
+    const elements = document.getElementsByClassName("animated-text");
     // console.log(elements)
     for (const el of elements) {
       const observer = new IntersectionObserver(
         ([entry]) => {
           if (entry.isIntersecting) {
             for (const c of entry.target.classList) {
-              if (String(c).includes('animated-text-delay')) {
-                const classBuffer = String(c).split('-')
-                const delay = classBuffer[classBuffer.length - 1]
-                entry.target.style.animationDelay = delay
+              if (String(c).includes("animated-text-delay")) {
+                const classBuffer = String(c).split("-");
+                const delay = classBuffer[classBuffer.length - 1];
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                entry.target.style.animationDelay = delay;
               }
             }
-            entry.target.classList.add("animate-[slide-in-up_1s_ease_forwards]")
+            entry.target.classList.add(
+              "animate-[slide-in-up_1s_ease_forwards]"
+            );
             observer.unobserve(entry.target); // Unobserve to trigger only once
           }
         },
         { threshold: 0.5 }
       );
-      el.classList.add("opacity-0")
-      observer.observe(el)
-      observers.push(observer)
+      el.classList.add("opacity-0");
+      observer.observe(el);
+      observers.push(observer);
     }
 
-    return () => observers.forEach(o => o.disconnect());
+    return () => observers.forEach((o) => o.disconnect());
   }, []);
 
   useEffect(() => {
-    const observers = []
-    const elements = document.getElementsByClassName("animated-card-image")
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const observers = [];
+    const elements = document.getElementsByClassName("animated-card-image");
     // console.log(elements)
     for (const el of elements) {
       const observer = new IntersectionObserver(
         ([entry]) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add("animate-[card-image-animation_1s_ease_forwards]")
+            entry.target.classList.add(
+              "animate-[card-image-animation_1s_ease_forwards]"
+            );
             observer.unobserve(entry.target); // Unobserve to trigger only once
           }
         },
         { threshold: 0.1 }
       );
-      el.classList.add("scale-125")
-      observer.observe(el)
-      observers.push(observer)
+      el.classList.add("scale-125");
+      observer.observe(el);
+      observers.push(observer);
     }
-
-    return () => observers.forEach(o => o.disconnect());
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return () => observers.forEach((o) => o.disconnect());
   }, []);
 
   return (
